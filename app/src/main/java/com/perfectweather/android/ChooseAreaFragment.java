d7f2cfeefaec64e0f6b3fb1de9ee6b4caeab0488
MrGaoDev <406967609@qq.com>
2017-08-19 15:27:16 +0800

    First commit.

---
+++ app/src/main/java/com/perfectweather/android/ChooseAreaFragment.java
@@ -0,0 +1,56 @@
+package com.perfectweather.android;
+
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.perfectweather.android.db.City;
+import com.perfectweather.android.db.County;
+import com.perfectweather.android.db.Province;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Boss on 2017/8/17.
+ */
+public class ChooseAreaFragment extends Fragment{
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<>();
+    /**
+     * 省列表
+     */
+    private List<Province> provinceList;
+    /**
+     * 市列表
+     */
+    private List<City> cityList;
+    /**
+     * 县列表
+     */
+    private List<County> countyList;
+
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/perfectweather/android/db/Province.java
@@ -0,0 +1,36 @@
+package com.perfectweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Boss on 2017/8/17.
+ */
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
